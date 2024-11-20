83b68ba098620e0fb9109bbc75836137b30c7413
bbqstand <mashrurfaiyaz@iut-dhaka.edu>
2024-11-20 14:59:47 +0600

    added new classes

---
+++ IceCreamFlavor.java
@@ -0,0 +1,17 @@
+public class IceCreamFlavor {
+    private String name;
+    private double pricePerScoop;
+
+    public IceCreamFlavor(String name, double pricePerScoop){
+        this.name = name;
+        this.pricePerScoop = pricePerScoop;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPricePerScoop() {
+        return pricePerScoop;
+    }
+}
